47edf988caa903d9f1bea1b4195df5a8c9b26533
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2019-07-05 15:19:30 +0200

    move Todo class from store.ts to todo.ts

---
+++ src/components/models/todo.ts
@@ -0,0 +1,30 @@
+import {action, observable} from "mobx";
+
+export class Todo {
+
+    public readonly id: number;
+
+    // Number of milliseconds from 1 January 1970 00:00:00
+    public readonly createdOn: number;
+
+    // Number of milliseconds from 1 January 1970 00:00:00
+    public completedOn: number;
+
+    public title: string;
+
+    @observable
+    public finished: boolean;
+
+
+    constructor(_title) {
+        this.title = _title;
+        this.id = Math.random();
+        this.createdOn = new Date().getTime()
+    }
+
+    @action
+    public toggleState() {
+        this.finished = !this.finished;
+        this.completedOn = this.finished ? new Date().getTime() : undefined
+    }
+}
