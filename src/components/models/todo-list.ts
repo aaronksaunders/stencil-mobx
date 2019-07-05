940678d7fb63b1e62dc6cdd5523feca17fab1d0f
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2019-07-05 15:21:37 +0200

    extract todo-list class from store.ts

---
+++ src/components/models/todo-list.ts
@@ -0,0 +1,33 @@
+import {action, extras, observable} from "mobx";
+import {Todo} from "./todo";
+
+export class TodoList {
+
+    [x: string]: any;
+
+    @observable
+    public todos: Todo[];
+
+    constructor() {
+        // this allows multiple independent mobx instances per page
+        // (good for isolated web components)
+        extras.isolateGlobalState();
+
+    }
+
+    public get unfinishedTodoCount() {
+        return this.todos
+            .slice()
+            .filter((todo: Todo) => !todo.finished).length
+    }
+
+    @action
+    public add(_value) {
+        this.todos.push(new Todo(_value))
+    }
+
+    @action
+    public removeTodo(_todo: Todo) {
+        this.todos = this.todos.slice().filter(p => p.id !== _todo.id);
+    }
+}
