ea4f9a98539c2263c156b0d83cacc503e6b7021b
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2019-07-05 18:41:15 +0200

    optimize visuals and functionality of app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,25 +1,46 @@
 // @ts-ignore
 import {Component, h, State} from '@stencil/core';
-import store from '../todo-list.store';
-import {computed} from 'mobx';
+import {autorun} from 'mobx';
+import {todoListStore} from "../todo-list.store";
+import {Todo} from "../models/todo";
 
 
 @Component({
     tag: 'my-app',
-    //styleUrl: 'my-name.scss'
+    styleUrl: 'app.scss'
 })
 export class App {
 
     @State() title: string;
+    @State() todos: Todo[];
 
     constructor() {
+
+        autorun(() => {
+            // this spread syntax has the advantage,
+            // that `this.todos` is never undefined
+            this.todos = [...todoListStore.todos];
+        })
     }
 
-    @computed
-    public get renderedTodos() {
-        return store.todos.map((m) => {
-            return (<div>{m.title} {new Date(m.createdOn).toISOString()}  {m.finished}</div>)
+    public renderTodos() {
+        return this.todos.map((m) => {
+            return (<tr>
+                <td>{m.title}</td>
+                <td>{new Date(m.createdOn).toISOString()}</td>
+                <td>{m.finished}</td>
+            </tr>)
         })
+    }
+
+    public handleAddClick(clickEvent: MouseEvent) {
+        clickEvent.preventDefault();
+
+        // add new \todo
+        todoListStore.add(this.title);
+
+        // reset title
+        this.title = '';
     }
 
     render() {
@@ -28,16 +49,31 @@ export class App {
                 <my-header></my-header>
                 <my-routes></my-routes>
                 <div class="level">
-                    <div class="level-item">
-                        <button class="button" onClick={() => store.add(this.title)}>ADD</button>
-                        <input class="input" placeholder="enter the title" onChange={(e: any) => {
+                    {/* This `form`-Tag allows the `Add`-Button to be triggered by pressing `Enter` on the keyboard */}
+                    <form class="level-item">
+                        <input class="input"
+                               placeholder="enter the title"
+                               value={this.title}
+                               onInput={(e: any) => {
                             this.title = e.target.value;
-                            console.log(e.target.value);
                         }} />
-                    </div>
-                    <h4 class="level-item">Unfinished: {store.unfinishedTodoCount}</h4>
+                        <button type="submit" class="button" onClick={this.handleAddClick.bind(this)}>ADD</button>
+                    </form>
+                    <h4 class="level-item">Unfinished: {todoListStore.unfinishedTodoCount}</h4>
                 </div>
-                {this.renderedTodos}
+
+                <table class="todo-table">
+                    <thead>
+                    <tr>
+                        <th>Todo Title</th>
+                        <th>Created At</th>
+                        <th>Finished</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.renderTodos()}
+                    </tbody>
+                </table>
             </div >
         )
     }
