18bbbc0ed067cc92fccadb2d109f5eb90c7414c3
Benjamin Jesuiter <benjamin.jesuiter@cgi.com>
2019-07-08 16:33:11 +0200

    add comment in render function for ui-todo-input

--- src/components/ui-todo-input/ui-todo-input.tsx
+++ src/components/ui-todo-input/ui-todo-input.tsx
@@ -25,6 +25,7 @@ export class UiTodoInput {
     render() {
         return (
             <form>
+                {/* This `form`-Tag allows the `Add`-Button to be triggered by pressing `Enter` on the keyboard */}
                 <input class="input" placeholder="Enter new Todo Title"
                        value={this.title} onInput={(e: any) => {
                     this.title = e.target.value;
